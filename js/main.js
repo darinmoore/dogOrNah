02b3c8d8908baedbb6e1baf4ff915ea4d9d56363
Darin Moore <dsmoore@ucsd.edu>
2016-10-01 16:27:56 -0700

    Fixed indentation

--- js/main.js
+++ js/main.js
@@ -11,9 +11,10 @@ function main() {
   
   var userScore = 0; // keeps track of user's score
   var randomIndex = 0; // random index to determine photo
-  var picture = 'dogOrNahPic'; // start of string for picture's name
+  var picture = 'pics/dogOrNawPic'; // start of string for picture's name
   var dog = false; // if the picture is actually a dog
   var correct = true; // if the user guessed correctly
+  var MAX_DOG = 23; // last dog picture index
 
   // Game keeps running as long as mistakes aren't made
   while (correct) {
@@ -23,7 +24,7 @@ function main() {
 
     var randomPic = picture + randomIndex + '.jpg';
 
-    if (randomIndex < 30) {
+    if (randomIndex < MAX_DOG) {
     	dog = true;
     }
     else {
@@ -37,36 +38,31 @@ function main() {
     userScore++;
   }
 
+}
+
 function checkKey() {    // ALLOW ARROW KEY INTERACTION    
     $(document).keydown(function(event) {
-	switch(event.which) {
-		// left arrowkey 
-		case 37:
-			if (dog) {
-				console.log("CORRECT - IT'S A DOG");
-			}
-			else {
-				console.log("U WRONG - NOT A DOG");
-				correct = false;
-			}
-			break;
-		// right arrowkey
-		case 39:
-			if (!dog) {
-				console.log("CORRECT - NOT DOG");
-			}
-			else {
-				console.log("U WRONG - IS DOG);
-				correct = false;
-			}
-			break;
-		// exit for other keys pressed
-		default:
-			return;
-	}
-	event.preventDefault();
+	    switch(event.which) {
+		    // left arrowkey 
+		    case 37:
+			    if (!dog) {
+				    correct = false;
+			    }
+			    break;
+		      
+        // right arrowkey
+		    case 39:
+			    if (dog) {
+				    correct = false;
+			    }
+		    	break;
+          
+		    // exit for other keys pressed
+		    default:
+			    return;
+	    }
+	    event.preventDefault();
     });
 };
 
 
-}
