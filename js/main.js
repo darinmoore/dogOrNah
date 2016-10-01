a29ab60c73910802fb4a62c0110358ce6975fd43
Darin Moore <dsmoore@ucsd.edu>
2016-10-01 11:56:23 -0700

    Began work on main.js

--- js/main.js
+++ js/main.js
@@ -8,6 +8,26 @@
  */
 
 function main() {
+  
+  var userScore = 0; // keeps track of user's score
+  var randomIndex = 0; // random index to determine photo
+  var picture = 'dogOrNahPic'; // start of string for picture's name
+
+  // Game keeps running as long as mistakes aren't made
+  while (true) {
+    
+    // chooses a random number to determine random picture
+    randomIndex = Math.floor(Math.random() * NUM_OF_PICS);
+
+    var randomPic = picture + randomIndex + '.jpg';
+
+    // if guess is incorrect, exits loop  
+    if (!correct) {
+      break;
+    }
+    userScore++;
+  }
+
 
 
 }
